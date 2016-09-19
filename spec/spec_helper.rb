8b18897fb73fdc3eddcac061e2c5603cbfb19f43
navin <navin@amagi.com>
2016-09-19 20:13:28 +0530

    keep workers in spec folder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,4 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-$LOAD_PATH.unshift File.expand_path('workers', __FILE__)
 require 'coveralls'
 Coveralls.wear!
 
@@ -10,4 +9,4 @@ Sidekiq.options[:redis_url] = {
 
 require 'sidekiq/addons/util'
 require 'sidekiq/addons'
-require 'workers/worker'
+require 'workers'
